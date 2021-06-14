7f2a3f5e7d266d7b4c1f3055148abdff33408ef6
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2021-06-14 02:27:58 +0200

    Rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/todos-ui'),
+      dir: require('path').join(__dirname, './coverage/hausrat-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
