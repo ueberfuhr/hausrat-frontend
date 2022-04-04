86007841fe7425e6849cdc7b7216b18e4c201a09
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-04-04 17:45:48 +0200

    Fix tests

--- src/app/toast/services/toast.service.spec.ts
+++ src/app/toast/services/toast.service.spec.ts
@@ -1,16 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ToastService } from './toast.service';
+import {ToastService} from './toast.service';
+import {Toast} from '../models/toast';
 
 describe('ToastService', () => {
-  let service: ToastService;
+    let service: ToastService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToastService);
-  });
+    beforeEach(() => {
+        // test the injectability of the service
+        service = TestBed.inject(ToastService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    // times out after 5000ms by default, if not emitted
+    it('should emit toasts', (done) => {
+        const toast: Toast = {message: 'test message'};
+        service.toasts.subscribe(t => {
+            expect(t).toBe(toast);
+            done();
+        });
+        service.show(toast);
+    });
+
 });
